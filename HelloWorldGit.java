316d01c764b081591b4ecaacfa78aeb24144b792
tanaya jadhav <tanaya_jadhav@persistent.co.in>
2018-07-26 15:39:06 +0530

    message improved and display date

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,9 +1,9 @@
 class HelloWorld{
 
         void displayDate(){
-           system.out.println("display today's date:-");
+           system.out.println("display today's date:-" +newdate());
    
-          System.out.println("hello world....welcome to git");
+          System.out.println("hello world!!!!!!!....welcome to git..!!!!!!!!");
              diplayDate();
  
       }
