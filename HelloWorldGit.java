c77bff4d9b2122dd333a9592bebc19a8a4707e04
tanaya jadhav <tanaya_jadhav@persistent.co.in>
2018-07-26 14:55:05 +0530

    displaydate method improved...sysout

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,13 +1,11 @@
 class HelloWorld{
 
-void displayDate()
-{
-system.out.println("display today's date:-");
-}
+        void displayDate(){
+           system.out.println("display today's date:-");
+      }
 
-public static void main(String[] args)
-{
-System.out.println("hello world....welcome to git")
-
-}
+             public static void main(String[] args){
+          System.out.println("hello world....welcome to git")
+ 
+      }
 }
