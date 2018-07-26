c62328875b8dedb7d05151354689140933c80b29
tanaya jadhav <tanaya_jadhav@persistent.co.in>
2018-07-26 15:24:22 +0530

    method calling conflict is fixed

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -2,16 +2,7 @@ class HelloWorld{
 
         void displayDate(){
            system.out.println("display today's date:-");
-      }
-<<<<<<<<<<<<<HEAD
-             public static void main(String[] args){
-                HelloWorld h = new Helloworld();
-h.helloworld();
-
-
-
-
-<<<<<<<<BUG456
+   
           System.out.println("hello world....welcome to git");
              diplayDate();
  
